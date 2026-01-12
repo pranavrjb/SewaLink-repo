818e6de4256fc823515a6cabc3f725b1eebfa8fb
pranavrjb <pranav.rjb79@gmail.com>
2026-01-12 22:44:54 +0545

    "fix route"

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { isAxiosError } from "axios";
 // Use relative path for ECS deployment
-const API_BASE_URL = /**"/api/v1"  ||**/ "http://localhost:5000/api/v1";
+const API_BASE_URL = "/api/v1"  /**|| "http://localhost:5000/api/v1" */;
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
