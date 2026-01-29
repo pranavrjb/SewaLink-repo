c2858c385decbdfff7ea81afc7e057a9ada879d1
pranavrjb <pranav.rjb79@gmail.com>
2026-01-30 00:13:17 +0545

    feat: Set up Kubernetes deployment and service configurations for frontend and backend, update API base URL, and comment out ECS service updates

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { isAxiosError } from "axios";
 // Use relative path for ECS deployment
-const API_BASE_URL = /**"/api/v1"  ||**/ "http://localhost:5000/api/v1";
+// const API_BASE_URL = /**"/api/v1"  ||**/ "http://localhost:5000/api/v1";
 // const API_BASE_URL = "/api/v1" /* || "http://localhost:5000/api/v1" */;
+const API_BASE_URL= "http://192.168.39.66:30005/api/v1"
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
